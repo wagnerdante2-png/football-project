8e4768cb55651ab39b3016343ad5275e76a5a452
wagnerdante2-png <wagnerdante2@gmail.com>
2026-08-24 23:55:08 -0300

    feat: add scalable public football data source catalog

---
+++ src/world-football-source-catalog-v1.ts
@@ -0,0 +1,19 @@
+import type { OpenFootballSource } from './openfootball-importer-v1';
+
+export type CountrySourceBundle={countryId:string;countryName:string;confederationId:string;leagueSources:OpenFootballSource[];clubSources:OpenFootballSource[];stadiumSources:OpenFootballSource[]};
+const src=(id:string,repo:string,path:string,kind:OpenFootballSource['kind'],countryId?:string,confederationId?:string):OpenFootballSource=>({id,repo,path,kind,countryId,confederationId,license:'CC0-1.0',historical:true});
+
+export const FOOTBALL_SOURCE_CATALOG:CountrySourceBundle[]=[
+ {countryId:'BRA',countryName:'Brasil',confederationId:'CONMEBOL',leagueSources:[src('of-bra-leagues','openfootball/leagues','south-america/brazil/br.leagues.txt','leagues','BRA','CONMEBOL')],clubSources:[src('of-bra-clubs','openfootball/clubs','south-america/brazil/br.clubs.txt','clubs','BRA','CONMEBOL')],stadiumSources:[src('of-bra-stadiums','openfootball/clubs','south-america/brazil/br.stadiums.txt','stadiums','BRA','CONMEBOL')]},
+ {countryId:'ENG',countryName:'Inglaterra',confederationId:'UEFA',leagueSources:[src('of-eng-leagues','openfootball/leagues','europe/england/eng.leagues.txt','leagues','ENG','UEFA')],clubSources:[src('of-eng-clubs','openfootball/clubs','europe/england/eng.clubs.txt','clubs','ENG','UEFA')],stadiumSources:[src('of-eng-stadiums','openfootball/clubs','europe/england/eng.stadiums.txt','stadiums','ENG','UEFA')]},
+ {countryId:'GER',countryName:'Alemanha',confederationId:'UEFA',leagueSources:[src('of-ger-leagues','openfootball/leagues','europe/germany/de.leagues.txt','leagues','GER','UEFA')],clubSources:[],stadiumSources:[]}
+];
+
+export const GLOBAL_FOOTBALL_SOURCES={
+ nationalAndInternational:src('of-global-leagues','openfootball/leagues','leagues.txt','leagues',undefined,undefined),
+ southAmericaInternational:src('of-conmebol-leagues','openfootball/leagues','south-america/leagues.txt','leagues',undefined,'CONMEBOL'),
+ europeInternational:src('of-uefa-leagues','openfootball/leagues','europe/leagues.txt','leagues',undefined,'UEFA')
+};
+
+export function sourceBundle(countryId:string){return FOOTBALL_SOURCE_CATALOG.find(x=>x.countryId===countryId)}
+export function allCatalogSources(){return[GLOBAL_FOOTBALL_SOURCES.nationalAndInternational,GLOBAL_FOOTBALL_SOURCES.southAmericaInternational,GLOBAL_FOOTBALL_SOURCES.europeInternational,...FOOTBALL_SOURCE_CATALOG.flatMap(x=>[...x.leagueSources,...x.clubSources,...x.stadiumSources])]}
